393d878c309f496732f708df9432c435738bbfd9
Thorsten Fischer <thorsten@froschi.org>
2012-05-04 16:42:04 +0200

    metadata.rb: Add supports clause.
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,5 @@ license          "Apache 2.0"
 description      "Installs/Configures libgpg-error"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
+%w/ubuntu/.each { |os| supports os }
